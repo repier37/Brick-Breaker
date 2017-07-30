092b07ddba77717446734bd13d1dbeea8b13f411
Repier37 <pierre37.gilles@gmail.com>
2017-07-30 18:19:03 +0200

    redone for eclipse config

---
+++ Brick-Breaker/src/BrickBreaker.cpp
@@ -0,0 +1,18 @@
+//============================================================================
+// Name        : qsdfqs.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <SDL.h>
+using namespace std;
+
+int main() {
+	int test;
+	cout << endl;
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
